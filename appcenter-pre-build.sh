f2cb732b75328c8288172e87c8e7e1c5a950afbf
sammyteahan <sammyteahan@gmail.com>
2019-05-13 09:11:08 -0600

    AppCenter CI/CD
    Add pre-build script with environment variables that will be pulled in
    based on branch name and appropriate .env file (e.g. .env.staging, etc)

---
+++ appcenter-pre-build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Creates an .env from existing env files for use with react-native-config
+# based on branch
+if [ "$APPCENTER_BRANCH" == "master" ]; then
+  cp .env.staging .env
+if [ "$APPCENTER_BRANCH" == "production" ]; then
+  cp .env.production .env
+else
+  cp .env.staging .env
+fi
+
+printf "\n.env created with contents:\n"
+cat .env
