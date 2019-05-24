c3cab525e44f22bb0b8cc54798a65e2142d75875
sammyteahan <sammyteahan@gmail.com>
2019-05-24 12:10:07 -0600

    Change color of initial react bridge screen and bump version

--- ios/submissioncounter/AppDelegate.m
+++ ios/submissioncounter/AppDelegate.m
@@ -28,7 +28,7 @@
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"submissioncounter"
                                             initialProperties:nil];
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:35.0f green:40.0f blue:55.0f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
