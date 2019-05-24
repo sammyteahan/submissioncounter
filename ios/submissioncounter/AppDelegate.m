13a35b7e6cc23c640e89782d1a847f4db82aef13
sammyteahan <sammyteahan@gmail.com>
2019-05-24 14:17:54 -0600

    Use proper UI color values

--- ios/submissioncounter/AppDelegate.m
+++ ios/submissioncounter/AppDelegate.m
@@ -28,7 +28,7 @@
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"submissioncounter"
                                             initialProperties:nil];
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:35.0f green:40.0f blue:55.0f alpha:1];
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:0.14 green:0.16 blue:0.22 alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
